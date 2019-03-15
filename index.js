c8ff8d793ba51dcb97fe3ec2c8d26606b656bd29
moleisking <moleisking@gmail.com>
2019-03-15 15:59:21 +0100

    trying crypto-js

--- index.js
+++ index.js
@@ -130,8 +130,8 @@ WooCommerceAPI.prototype._getOAuth = function() {
     },
     signature_method: 'HMAC-SHA256',
     hash_function: function(base_string, key) {
-        return crypto.createHmac('sha256', key).update(base_string)
-          .digest('base64');
+        //return crypto.createHmac('sha256', key).update(base_string).digest('base64');
+        return CryptoJS.HmacSHA256(base_string, key).toString(CryptoJS.enc.Base64);
     }
   };
 
